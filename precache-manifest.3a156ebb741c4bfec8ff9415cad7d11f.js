69b19cafd6d7f336e6cefe435f024a287f3c3a5f
Serkan Ferat <alisener94@yahoo.com>
2020-08-08 01:46:18 +0300

    Updates

---
+++ precache-manifest.3a156ebb741c4bfec8ff9415cad7d11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b5304624640b555277ce3c90f4c867",
+    "url": "/nike/index.html"
+  },
+  {
+    "revision": "f6286055cb405adcf361",
+    "url": "/nike/static/css/main.d3ad2fef.chunk.css"
+  },
+  {
+    "revision": "0b3fa4defadb7d29b944",
+    "url": "/nike/static/js/2.87f24480.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nike/static/js/2.87f24480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6286055cb405adcf361",
+    "url": "/nike/static/js/main.1523bd08.chunk.js"
+  },
+  {
+    "revision": "8a145725ee38f97e8008",
+    "url": "/nike/static/js/runtime-main.5701da84.js"
+  }
+]);
